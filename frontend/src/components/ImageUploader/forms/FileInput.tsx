d1a2ee98e1fe25833491cacecae69b1f252a682b
RiCoDeKu <131886197+RiCoDeKu@users.noreply.github.com>
2024-12-08 14:12:28 +0900

    Fixed image acceptance settings.

--- frontend/src/components/ImageUploader/forms/FileInput.tsx
+++ frontend/src/components/ImageUploader/forms/FileInput.tsx
@@ -3,19 +3,19 @@ interface FileInputProps {
 }
 
 const FileInput: React.FC<FileInputProps> = ({ onChange }) => (
-  <>
-    {/* 画像アップロードのラベル */}
+<>
+	{/* 画像アップロードのラベル */}
 	<a className="mt-4 block text-lg font-semibold">画像を追加</a>
-    {/* 画像ファイル選択フィールド */}
-    <input
-      id="imageFile"
-      name="imageFile"
-      type="file"
-      accept=".png, .jpeg, .jpg .heic"
-      onChange={(e) => onChange(e.target.files ? e.target.files[0] : null)}
-      className="w-full p-2 border border-gray-300 rounded"
-    />
-  </>
+	{/* 画像ファイル選択フィールド */}
+	<input
+		id="imageFile"
+		name="imageFile"
+		type="file"
+		accept="image/*"
+		onChange={(e) => onChange(e.target.files ? e.target.files[0] : null)}
+		className="w-full p-2 border border-gray-300 rounded"
+	/>
+</>
 );
 
 export default FileInput;
